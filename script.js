309ec2b056b702a741b5d812545c2d29f743a091
Gabriel Ramirez <gramirez@webgdesigns.com>
2024-01-10 02:12:12 -0600

    Created Quote Generator

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log('test');
+const quoteText = document.getElementById('quoteText');
+const authorText = document.getElementById('authorText');
+const newQuoteBtn = document.getElementById('newQuoteBtn');
+
+
+let apiData = [];
+
+// Get Quotes from API
+async function getData() {
+    const apiUrl = 'quotes.json';
+
+    // Attempt to fetch if not catch error
+    try {
+        const response = await fetch(apiUrl);
+        apiData = await response.json(); //Converts response to a json
+        console.log(apiData);
+    } catch (error){
+        alert(error);
+    }
+}
+
+// When page loads run function
+getData();
+
+//Display new quote
+function newQuote(){
+    const quote = apiData[Math.floor(Math.random() * apiData.length)];
+    quoteText.textContent = quote.text;
+    authorText.textContent =quote.author;
+}
+
+newQuoteBtn.addEventListener('click', newQuote);
